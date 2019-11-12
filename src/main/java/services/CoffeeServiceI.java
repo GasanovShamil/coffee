4654eed2b7f06dea8d1ae44cda2fd6b2d260639f
Gasanov <shamil.gasanov@cgi.com>
2019-11-12 10:39:19 +0100

    first commit

---
+++ src/main/java/services/CoffeeServiceI.java
@@ -0,0 +1,6 @@
+package services;
+
+public interface CoffeeServiceI {
+
+    public void makeCoffee(Integer drink, Integer sugar);
+}
---
+++ src/main/java/services/CoffeeServiceImpl.java
@@ -0,0 +1,8 @@
+package services;
+
+public class CoffeeServiceImpl implements CoffeeServiceI {
+
+    public void makeCoffee(Integer drink, Integer sugar) {
+
+    }
+}
